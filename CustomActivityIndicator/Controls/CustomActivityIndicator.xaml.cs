2f968759829d458743fa263d472ee4db815b85ce
jorgediegocrespo <jorgediegocrespo@gmail.com>
2019-09-02 19:20:48 +0200

    Custom activity indicator control

---
+++ CustomActivityIndicator/Controls/CustomActivityIndicator.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace CustomActivityIndicator.Controls
+{
+    using Xamarin.Forms;
+
+    public partial class CustomActivityIndicator : ContentView
+    {
+        private readonly Animation animation;
+        private const string ANIMATION_NAME = "ActivityIndicatorRotation";
+
+        public static readonly BindableProperty IsRunningProperty = BindableProperty.Create(nameof(IsRunning), typeof(bool), typeof(CustomActivityIndicator), false, propertyChanged: IsRunningChanged);
+        public static readonly BindableProperty SourceProperty = BindableProperty.Create(nameof(Source), typeof(ImageSource), typeof(CustomActivityIndicator), default(ImageSource));
+
+        public CustomActivityIndicator()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            animation = new Animation(v => Rotation = v, 0, 360);
+        }
+
+        public bool IsRunning
+        {
+            get => (bool)GetValue(IsRunningProperty);
+            set => SetValue(IsRunningProperty, value);
+        }
+
+        public ImageSource Source
+        {
+            get => (ImageSource)GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+
+        private static void IsRunningChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if ((bool)newValue)
+                ((CustomActivityIndicator)bindable).StartAnimation();
+            else
+                ((CustomActivityIndicator)bindable).StopAnimation();
+        }
+
+        private void StartAnimation()
+        {
+            IsVisible = true;
+            this.ScaleTo(1, 500);
+            animation.Commit(this, ANIMATION_NAME, 16, 1000, Easing.Linear, (v, c) => Rotation = 0, () => true);
+        }
+
+        private async void StopAnimation()
+        {
+            await this.ScaleTo(0, 500);
+            this.AbortAnimation(ANIMATION_NAME);
+            IsVisible = false;
+        }
+    }
+}
---
+++ CustomActivityIndicator/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CustomActivityIndicator
+{
+    using System;
+
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            BtnChangeBusyIndicator.Clicked += BtnChangeBusyIndicator_Clicked;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            BtnChangeBusyIndicator.Clicked -= BtnChangeBusyIndicator_Clicked;
+        }
+
+        private void BtnChangeBusyIndicator_Clicked(object sender, EventArgs e)
+        {
+            BusyIndicator.IsRunning = !BusyIndicator.IsRunning;
+            BtnChangeBusyIndicator.Text = BusyIndicator.IsRunning ? "Stop" : "Run";
+        }
+    }
+}
